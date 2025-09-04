312b3b2946cb7a10bcfa2dc2a5661c7e2448e237
pasteldemisto <jdsouz@edu.unifor.br>
2025-09-04 00:34:18 -0300

    add frequency graus

--- MatrizIncidencia.py
+++ MatrizIncidencia.py
@@ -3,7 +3,7 @@ class MatrizIncidencia:
     def __init__(self, num_vertices, arestas):
         self.num_vertices = num_vertices
         self.arestas = arestas
-        self._matriz = []
+        self.matriz = []
 
     def criar_matriz_incidencia(self):
         num_arestas = len(self.arestas)
@@ -20,7 +20,7 @@ class MatrizIncidencia:
         for linha in self.matriz:
             print(linha)
 
-    def _contar_fronteiras(self):
+    def _contar_arestas_de_cada_vertice(self):
         return [sum(linha) for linha in self.matriz]
     
     def _vizinhanca(self, v):
@@ -33,26 +33,36 @@ class MatrizIncidencia:
         return sorted(set(vizinhos))
 
     def maior_ocorrencia(self, dicionario):
-        contagens = self._contar_fronteiras()
+        contagens = self._contar_arestas_de_cada_vertice()
         maior = max(contagens)
         vertices = [i for i, c in enumerate(contagens) if c == maior]
 
         for v in vertices:
             fronteiras = self._vizinhanca(v)
-            print(f"Vértice {dicionario[v]} tem {maior} arestas: ")
+            print(f"(Grau Máximo) Vértice {dicionario[v]} tem {maior} arestas: ")
             for chave in fronteiras:
                 print("- " + dicionario[chave])
 
 
     def menor_ocorrencia(self, dicionario):
-        contagens = self._contar_fronteiras()
+        contagens = self._contar_arestas_de_cada_vertice()
         menor = min(contagens)
         vertices = [i for i, c in enumerate(contagens) if c == menor]
 
         for v in vertices:
             fronteiras = self._vizinhanca(v)
-            print(f"Vértice {dicionario[v]} tem {menor} arestas: ")
+            print(f"(Grau Mínimo) Vértice {dicionario[v]} tem {menor} arestas: ")
             for chave in fronteiras:
                 print("- " + dicionario[chave])
-
-
+                
+    def frequencia_graus(self):
+        contagens = self._contar_arestas_de_cada_vertice()
+        freq = {}
+        for grau in contagens:
+            if grau in freq:
+                freq[grau] += 1
+            else:
+                freq[grau] = 1
+                
+        print(freq)
+        return freq
